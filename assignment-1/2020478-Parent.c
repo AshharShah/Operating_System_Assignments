e30dd18e56d2dbf9cf4e1c88c3a80f92819823b9
AshharShah <ashhar.shah786@gmail.com>
2022-09-22 17:37:05 +0500

    Setup Base Child and Parent Files

--- assignment-1/2020478-Parent.c
+++ assignment-1/2020478-Parent.c
@@ -3,9 +3,26 @@
 
 int main(int argc, char* argv[]){
 
+    // Part 1, parent given 2 files as arguments which are displayed
     printf("The names of the files passed are:\n");
     for(int i = 1; i < argc; i++){
         printf("%s\n", argv[i]);
     }
-    exit(1);
+
+    // Creating a child via the fork command
+    int pid = fork();
+
+    if(pid == -1){
+        // what to do if failure to fork occurs (error handling)
+        printf("Fork Failed!\n");
+        exit(NULL);
+    }
+    else if(pid == 0){
+        // code for the child
+    }
+    else{
+        // code for the parent
+    }
+
+    exit(NULL);
 }
